cc032c2472adbc56e01b1c0f95cde45dec035314
Felipe Bregalda <felipedalpontebregalda@gmail.com>
2017-09-28 14:21:05 -0300

    Calculo da nota final do aluno

---
+++ funcoes.py
@@ -0,0 +1,2 @@
+def notaFinal(nota1,nota2):
+    return((nota1+(nota2*2))/3)
--- notas.py
+++ notas.py
@@ -1,5 +1,11 @@
+#-*- coding utf-8 -*-
+from funcoes import notaFinal
+
 print 'Calculo de notas do Aluno\n'
 
 nome = raw_input ('Nome: ')
 nota1 = float( raw_input('Nota 1: '))
 nota2 = float( raw_input('Nota 2: '))
+
+notaFinal = notaFinal(nota1, nota2)
+print 'Nota final do aluno',nome,'eh',notaFinal
